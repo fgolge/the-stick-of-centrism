b872d3b847412277532dafcfeb24330f3848ab7e
fgolge <steamgolge@gmail.com>
2021-06-05 09:57:10 +0300

    1.01

---
+++ The Stick Of Centrism/Assets/Scripts/MoveStick.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStick : MonoBehaviour
+{
+    private Vector3 startPosition = new Vector3(0f, 0f, 0f);
+
+    
+
+    
+}
