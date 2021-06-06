bfcdd331bd0ce3269e4f8b49795630457aca489c
fgolge <steamgolge@gmail.com>
2021-06-06 16:05:53 +0300

    0.1.1
    -fixed player control bugs
    -added boundaries
    -added ball

--- The Stick Of Centrism/Assets/Scripts/MoveStick.cs
+++ The Stick Of Centrism/Assets/Scripts/MoveStick.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class MoveStick : MonoBehaviour
 {
-    private Vector3 startPosition = new Vector3(0f, 0f, 0f);
+    private Vector3 startPosition = new Vector3(0f, 0.129f, 0f);
+
+    
+
+    // Reference points of edges of the stick
     private Vector3 leftEdge;
     private Vector3 rightEdge;
+    private Vector3 initPos;
 
     [SerializeField] private float moveSpeed = 1.0f;
     [SerializeField] private float rotateSpeed = 10.0f;
@@ -14,61 +19,132 @@ public class MoveStick : MonoBehaviour
     private float maxRotate = 100f;
     private float minRotate = 80f;
 
+    private float upperBound = 1.85f;
+
     private void Start()
     {
         transform.position = startPosition;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        // Direction booleans
+        bool isLeftUp = Input.GetKey(KeyCode.W);
+        bool isLeftDown = Input.GetKey(KeyCode.S);
+        bool isRightUp = Input.GetKey(KeyCode.O);
+        bool isRightDown = Input.GetKey(KeyCode.L);
+
         Vector3 offset = transform.up * (transform.localScale.y);
         leftEdge = transform.position + offset;
         rightEdge = transform.position - offset;
 
+        // Limit of rotation of the stick
         Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x, 0f, 0f);
         transform.position = pos;
 
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.O))
+        if (isLeftUp && isRightUp)
         {
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+            if (!((transform.GetChild(0).position.y) > upperBound) && !((transform.GetChild(1).position.y) > upperBound))
+            {
+                MoveUp();
+            }
         }
-        else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.O))
+        else if (isLeftUp && !isRightUp)
         {
-            float angleZ = transform.rotation.eulerAngles.z;
-            angleZ -= rotateSpeed * Time.deltaTime;
-            angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
-            transform.RotateAround(rightEdge, Vector3.back, rotateSpeed * Time.deltaTime);
-            transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
+            if (!((transform.GetChild(1).position.y) > upperBound))
+            {
+                MoveLefttSideUp();
+            }
         }
-        else if (Input.GetKey(KeyCode.O) && !Input.GetKey(KeyCode.W))
+        else if (isRightUp && !isLeftUp)
         {
-            float angleZ = transform.rotation.eulerAngles.z;
+            if (!((transform.GetChild(0).position.y) > upperBound))
+            {
+                MoveRightSideUp();
+            }
+        }
+
+        if (isLeftDown && isRightDown)
+        {
+            if (!((transform.GetChild(0).position.y) < startPosition.y) && !((transform.GetChild(1).position.y) < startPosition.y))
+            {
+                MoveDown();
+            }
+        }
+        else if (isLeftDown && !isRightDown)
+        {
+            if (!((transform.GetChild(1).position.y) < startPosition.y))
+            {
+                MoveLeftSideDown();
+            }
+        }
+        else if (isRightDown && !isLeftDown)
+        {
+            if (!((transform.GetChild(0).position.y) < startPosition.y))
+            {
+                MoveRightSideDown();
+            }
+        }
+    }
+
+    private void MoveUp()
+    {
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void MoveRightSideUp()
+    {
+        float angleZ = transform.rotation.eulerAngles.z;
+        angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
+        if (angleZ != maxRotate)
+        {
             angleZ += rotateSpeed * Time.deltaTime;
-            angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
             transform.RotateAround(leftEdge, Vector3.forward, rotateSpeed * Time.deltaTime);
             transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
         }
+    }
 
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.L))
+    private void MoveRightSideDown()
+    {
+        float angleZ = transform.rotation.eulerAngles.z;
+        angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
+        if (angleZ != minRotate)
         {
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
-        }
-        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.L))
-        {
-            float angleZ = transform.rotation.eulerAngles.z;
-            angleZ += rotateSpeed * Time.deltaTime;
-            angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
-            transform.RotateAround(rightEdge, Vector3.forward, rotateSpeed * Time.deltaTime);
-            transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
-        }
-        else if (Input.GetKey(KeyCode.L) && !Input.GetKey(KeyCode.S))
-        {
-            float angleZ = transform.rotation.eulerAngles.z;
             angleZ -= rotateSpeed * Time.deltaTime;
-            angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
             transform.RotateAround(leftEdge, Vector3.back, rotateSpeed * Time.deltaTime);
             transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
+        }
+    }
+
+    private void MoveLefttSideUp()
+    {
+        float angleZ = transform.rotation.eulerAngles.z;
+        angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
+        if (angleZ != minRotate)
+        {
+            angleZ -= rotateSpeed * Time.deltaTime;
+            transform.RotateAround(rightEdge, Vector3.back, rotateSpeed * Time.deltaTime);
+            transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
+
+        }
+    }
+
+    private void MoveLeftSideDown()
+    {
+        float angleZ = transform.rotation.eulerAngles.z;
+        angleZ = Mathf.Clamp(angleZ, minRotate, maxRotate);
+        if (angleZ != maxRotate)
+        {
+            angleZ += rotateSpeed * Time.deltaTime;
+            transform.RotateAround(rightEdge, Vector3.forward, rotateSpeed * Time.deltaTime);
+            transform.localRotation = Quaternion.Euler(0f, 0f, angleZ);
+
         }
     }
 }
